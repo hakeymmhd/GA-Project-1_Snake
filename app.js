8fa6ef04034054821256334a48e37c53c0492c72
mhdhakeym <mhdhakeym786@gmail.com>
2021-09-22 21:48:11 +0800

    created initial snake body and draw function

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ $(() => {
             this.dx = dx;
             this.dy = dy;
     
-            this.body = [
+            this.body = [           // each player starts with body length 5
               {x: 200, y: 200},
               {x: 190, y: 200},
               {x: 180, y: 200},
@@ -19,5 +19,16 @@ $(() => {
               {x: 160, y: 200},
             ];
           }
+
+          drawSnake() {
+            //console.log(this);
+            this.body.forEach((cb) => {
+              context.fillStyle = this.color;       // diff colors for each player
+              context.strokestyle = 'black';
+              context.fillRect(cb.x, cb.y, 10, 10);
+              context.strokeRect(cb.x, cb.y, 10, 10);
+            });
+          }
     }
+
 })
