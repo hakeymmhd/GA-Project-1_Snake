2a853922643d64dcf2717c7a1d39a5e793b8b392
mhdhakeym <mhdhakeym786@gmail.com>
2021-09-22 21:42:23 +0800

    linked canvas and created Reptile class

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 console.log("links established");
 
 $(() => {
+    const canvas = document.getElementById("canvas1");
+    const context = canvas.getContext("2d");
+
+    class Reptile {
+        constructor(name, color, dx, dy) {
+            this.name = name;
+            this.color = color;
+            this.dx = dx;
+            this.dy = dy;
     
+            this.body = [
+              {x: 200, y: 200},
+              {x: 190, y: 200},
+              {x: 180, y: 200},
+              {x: 170, y: 200},
+              {x: 160, y: 200},
+            ];
+          }
+    }
 })
