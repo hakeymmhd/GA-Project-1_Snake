10e408485638295861d03759709e595a91e3228f
mhdhakeym <mhdhakeym786@gmail.com>
2021-09-25 14:18:49 +0800

    implemented inter-player collision

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ $(() => {
     let foodFlag = false;
 
     class Reptile {
-        constructor(name, color, dx, dy, changing_direction, left, up, right, down) {
+        constructor(name, color, dx, dy, changing_direction, left, up, right, down, oriX, oriY) {
             this.name = name;
             this.color = color;
             this.dx = dx;           // snake horizontal movement
@@ -21,11 +21,11 @@ $(() => {
             this.goDown = down;
             
             this.body = [           // each player starts with body length 5
-              {x: 200, y: 200},     // starts snake at middle of 400x400 canvas
-              {x: 190, y: 200},
-              {x: 180, y: 200},
-              {x: 170, y: 200},
-              {x: 160, y: 200},
+              {x: oriX, y: oriY},     // starts snake at middle of 400x400 canvas
+              {x: oriX - 10, y: oriY - 10},
+              {x: oriX - 20, y: oriY - 20},
+              {x: oriX - 30, y: oriY - 30},
+              {x: oriX - 40, y: oriY - 40},
             //   {x: 150, y: 200},
             //   {x: 140, y: 200},
             //   {x: 130, y: 200},
@@ -45,7 +45,7 @@ $(() => {
           move_snake() {
             const addHead = {x: this.body[0].x + this.dx, y: this.body[0].y + this.dy};
             this.body.unshift(addHead);  // prepends new head in front of body and then removes the tail end
-            console.log(`${food_x} - ${food_y}`);
+            // console.log(`${food_x} - ${food_y}`);
             if (this.body[0].x === food_x && this.body[0].y === food_y) {
                 foodGen(this);
             } else {
@@ -89,25 +89,35 @@ $(() => {
             }
         }
 
-        gameStatusFlag() {  // figure how to implement inter-player collision detection 
+        gameStatusFlag(competitor) {  // figure how to implement inter-player collision detection 
                             // maybe a for loop outside the class and check snake1.body[0] against all of snake2.body
-            for (let i = 1; i < this.body.length; i++) {  // check for self-collision
+            for (let i = 3; i < this.body.length; i++) {  // check for self-collision
                 if (this.body[i].x === this.body[0].x && this.body[i].y === this.body[0].y) return true  //checks if snake has hit itself
             }
+            // console.log(competitor);
+            for (let j = 0; j<competitor.length; j++) {    //check for inter-player collision
+               
+                if (this.body[0].x === competitor[j].x && this.body[0].y === competitor[j].y) return true; 
+            }
+
             const leftLimit = this.body[0].x < 0;     // creates flag and returns it
             const rightLimit = this.body[0].x > canvas.width - 10;
             const topLimit = this.body[0].y < 0;
             const bottomLimit = this.body[0].y > canvas.height - 10;
             return leftLimit || rightLimit || topLimit || bottomLimit
         }   
+
+        getSnakePos () {
+            return this.body;
+        }
     }
 
     class Game {
         
     }
 
-    const snake1 = new Reptile('Player1', 'red', 10, 0, false, 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown');
-    const snake2 = new Reptile('Player2', 'yellow', 10 , 10, false, 'a', 'w', 'd', 's');
+    const snake1 = new Reptile('Player1', 'red', 10, 0, false, 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown', 200, 200);
+    const snake2 = new Reptile('Player2', 'yellow', 10 , 10, false, 'a', 'w', 'd', 's', 100, 100);
 
     const clearCanvas = () => {        // removes the deleted tail from snake's array of objects
         context.fillStyle = 'white';
@@ -149,7 +159,8 @@ $(() => {
     });
 
     const main = () => {
-        if (snake1.gameStatusFlag() || snake2.gameStatusFlag()) return;
+        // console.log(snake1.getSnakePos());
+        if (snake1.gameStatusFlag(snake2.getSnakePos()) || snake2.gameStatusFlag(snake1.getSnakePos())) return;
         
         else {
             setTimeout(() => {
